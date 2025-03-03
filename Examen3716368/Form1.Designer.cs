75115cf608dcffb642ff2b03b85f9f240a648d1e
Elizabeth <estrellitacalderon128@gmail.com>
2025-03-02 21:27:29 -0600

    Agregar archivos de proyecto.

---
+++ Examen3716368/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Examen3716368
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNie = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("High Tower Text", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(108, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EXAMEN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NIE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingresa la nota del examen:";
+            // 
+            // txtNie
+            // 
+            this.txtNie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNie.Location = new System.Drawing.Point(50, 44);
+            this.txtNie.Name = "txtNie";
+            this.txtNie.Size = new System.Drawing.Size(56, 22);
+            this.txtNie.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(71, 70);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(235, 22);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtNota
+            // 
+            this.txtNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota.Location = new System.Drawing.Point(171, 123);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(48, 22);
+            this.txtNota.TabIndex = 6;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(12, 188);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(99, 34);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(12, 228);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(99, 34);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsulta.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsulta.Location = new System.Drawing.Point(12, 268);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(99, 34);
+            this.btnConsulta.TabIndex = 9;
+            this.btnConsulta.Text = "Consulta";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(12, 308);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(99, 34);
+            this.btnModificar.TabIndex = 10;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(131, 250);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 28);
+            this.btnAceptar.TabIndex = 11;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(212, 250);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 28);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMostrar.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(232, 121);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 13;
+            this.btnMostrar.Text = "Calificar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(232, 314);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 28);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Fax", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(-12, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(341, 14);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "------------------------ FUNCIONES DE LA BASE --------------------------\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(318, 348);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtNie);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNie;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtNota;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Examen3716368/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen3716368
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Data.OleDb.OleDbConnection conBD = new System.Data.OleDb.OleDbConnection();
+
+        String sAccion = "";
+
+        public void abrirConexion()
+        {
+            conBD.ConnectionString = @"Provider=Microsoft.JET.OLEDB.4.0;  Data Source=C:\Users\estre\OneDrive\Escritorio\Practica3\crudExamen\examenBd.mdb";
+
+            try
+            {
+                conBD.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexion " + ex);
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (conBD.State == ConnectionState.Open)
+            {
+                conBD.Close();
+            }
+        }
+
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNota.Text, out double nota))
+            {
+                if (nota > 6)
+                {
+                    MessageBox.Show("Aprobado", "Resultado");
+                }
+                else
+                {
+                    MessageBox.Show("Reprobado", "Resultado");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nota inválida", "Error");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            abrirConexion();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            cerrarConexion();
+
+            this.Close();
+        }
+
+        private void desactivarAcciones()
+        {
+            btnConsulta.Enabled = false;
+            btnAgregar.Enabled = false;
+            btnEliminar.Enabled = false;
+            btnModificar.Enabled = false;
+            btnAceptar.Enabled = true;
+            btnCancelar.Enabled = true;
+        }
+
+        private void activarAcciones()
+        {
+            btnConsulta.Enabled = true;
+            btnAgregar.Enabled = true;
+            btnEliminar.Enabled = true;
+            btnModificar.Enabled = true;
+            btnAceptar.Enabled = false;
+            btnCancelar.Enabled = false;
+
+            sAccion = "";
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+            sAccion = "ConsultarR";
+
+            desactivarAcciones();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            sAccion = "AgregarR";
+
+            desactivarAcciones();
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            sAccion = "EliminarR";
+
+            desactivarAcciones();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            sAccion = "ModificarR";
+
+            desactivarAcciones();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            activarAcciones();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (sAccion == "ConsultarR")
+            {
+                try
+                {
+                    if (Convert.ToInt32(txtNie.Text) > 0)
+                    {
+                        string sConsulta = "SELECT * " + "FROM Registros " + "WHERE Nie= " + txtNie.Text;
+                        OleDbCommand oleComando = new OleDbCommand(sConsulta, conBD);
+                        OleDbDataReader oleReader = oleComando.ExecuteReader();
+
+                        if (oleReader.Read())
+                        {
+                            txtNombre.Text = Convert.ToString(oleReader["Nombre"]);
+
+                            txtNota.Text = Convert.ToString(oleReader["Nota"]);
+                        }
+                        else
+                        {
+                            MessageBox.Show("El codigo introducido no existe.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Introducir un codigo válido antes de consultar.");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Introducir un codigo antes de consultar.");
+                }
+            }
+            else
+            {
+                if (sAccion == "AgregarR")
+                {
+                    try
+                    {
+                        string sConsulta = "INSERT INTO Registros (Nombre, Nota) VALUES   (' " + txtNombre.Text + "', " + txtNota.Text + " )";
+
+                        OleDbCommand oleComando = new OleDbCommand(sConsulta, conBD);
+                        oleComando.CommandType = CommandType.Text;
+                        oleComando.CommandText = sConsulta;
+                        oleComando.Connection = conBD;
+
+                        oleComando.ExecuteNonQuery();
+
+                        txtNie.Text = "";
+                        txtNombre.Text = "";
+                        txtNota.Text = "";
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error en la inserccion" + ex.Message);
+                    }
+                }
+                else
+                {
+                    if (sAccion == "EliminarR")
+                    {
+                        try
+                        {
+                            String sConsulta = "DELETE FROM Registros WHERE Nie = " + txtNie.Text;
+                            OleDbCommand oleComando = new OleDbCommand();
+                            oleComando.CommandType = CommandType.Text;
+                            oleComando.CommandText = sConsulta;
+                            oleComando.Connection = conBD;
+
+                            oleComando.ExecuteNonQuery();
+                            txtNie.Text = "";
+                            txtNombre.Text = "";
+                            txtNota.Text = "";
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error en el borrado" + ex.Message);
+
+                        }
+                    }
+
+                    else
+                    {
+                        try
+                        {
+                            String sConsulta = "UPDATE Registros SET Nombre = '" + txtNombre.Text + "' ,  Nota = '" + txtNota.Text + "' WHERE Nie = " + txtNie.Text;
+                            OleDbCommand oleComando = new OleDbCommand();
+
+                            oleComando.CommandType = CommandType.Text;
+                            oleComando.CommandText = sConsulta;
+                            oleComando.Connection = conBD;
+                            oleComando.ExecuteNonQuery();
+
+                            txtNie.Text = "";
+                            txtNombre.Text = "";
+                            txtNota.Text = "";
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error al modificar" + ex.Message);
+                        }
+                    }
+                }
+            }
+            sAccion = "";
+
+            activarAcciones();
+        }
+    }
+}
